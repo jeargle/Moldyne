b7e0bd35c1eee6c50d82028d01f69da8972d20f5
John Eargle <jeargle@gmail.com>
2017-10-14 16:39:48 -0500

    #3 Structures can be built in julia

---
+++ test/test.jl
@@ -0,0 +1,33 @@
+# John Eargle (mailto: jeargle at gmail.com)
+# 2017
+# Moldyne test
+
+using Moldyne
+
+
+function test_structure()
+    println("***")
+    println("*** Structure")
+    println("***")
+
+    structure1 = Structure("structure1", 2)
+    structure2 = Structure("structure2", 3)
+
+    println(structure1)
+    println(structure2)
+    println()
+
+    setPositions(structure1, "test2.pdb")
+    setPositions(structure2, "test2.pdb")
+
+    println(structure1)
+    println(structure2)
+    println()
+end
+
+
+function main()
+    test_structure()
+end
+
+main()
