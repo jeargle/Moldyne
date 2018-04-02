d1748ff050339c06e0c9105c6a0cc58a6243add3
John Eargle <jeargle@gmail.com>
2018-04-01 19:52:43 -0500

    #3 pulled setPositions() into Structure constructor

--- src/moldyne.jl
+++ src/moldyne.jl
@@ -11,83 +11,27 @@ type Structure
     name::AbstractString
     dimension::Int64
     positions::Array{Array{Float64, 1}, 1}
-    Structure(name::AbstractString, dimension) = new(name, dimension, [])
-end
+    function Structure(name::AbstractString, dimension, filename)
+        positions = Array{Float64, 1}[]
+        open(filename, "r") do f
+            for line in eachline(f)
+                println(line)
+                println(line[31:38])
+                println(line[39:46])
+                c1 = float(line[31:38])
+                c2 = float(line[39:46])
 
-# Read PDB file and extract atomic positions.
-# filename: input PDB file
-function setPositions(structure, filename)
-    open(filename, "r") do f
-        for line in eachline(f)
-            println(line)
-            println(line[31:38])
-            println(line[39:46])
-            c1 = float(line[31:38])
-            c2 = float(line[39:46])
-
-            if structure.dimension == 2
-                push!(structure.positions, [c1, c2])
-            elseif structure.dimension == 3
-                c3 = float(line[47:54])
-                push!(structure.positions, [c1, c2, c3])
+                if dimension == 2
+                    push!(positions, [c1, c2])
+                elseif dimension == 3
+                    c3 = float(line[47:54])
+                    push!(positions, [c1, c2, c3])
+                end
             end
         end
+        new(name, dimension, positions)
     end
 end
 
 
 end
-
-
-
-# class Structure
-
-#   attr_reader :positions
-
-#   #
-#   def initialize(structureFilename, dimension)
-#     @structureFilename = structureFilename
-#     @dimension = dimension
-#     numPositions = getNumPositions
-#     @positions = Array.new(numPositions)
-#     setPositions
-#   end
-
-#   # @return Number of atoms in the simulation
-#   def numAtoms
-#     return @positions.length
-#   end
-
-#   # Get number of atomic positions defined in @structureFile
-#   # @return Number of lines in the file
-#   def getNumPositions
-#     numLines = 0
-
-#     File.readlines(@structureFilename).each do |line|
-#       numLines += 1
-#     end
-
-#     return numLines
-#   end
-
-#   # Read PDB file and extract atomic positions.
-#   def setPositions
-#     numLines = 0
-
-#     File.readlines(@structureFilename).each do |line|
-#       c1 = line[31,7].to_f
-#       c2 = line[39,7].to_f
-
-#       if @dimension == 2
-#         @positions[numLines] = Coord2d.new(c1, c2)
-#       elsif @dimension == 3
-#         c3 = line[47,7].to_f
-#         @positions[numLines] = Coord3d.new(c1, c2, c3)
-#       end
-
-#       numLines += 1
-#     end
-
-#   end
-
-# end
--- test/test.jl
+++ test/test.jl
@@ -1,5 +1,5 @@
 # John Eargle (mailto: jeargle at gmail.com)
-# 2017
+# 2017-2018
 # Moldyne test
 
 using moldyne
@@ -10,15 +10,8 @@ function test_structure()
     println("*** Structure")
     println("***")
 
-    structure1 = Structure("structure1", 2)
-    structure2 = Structure("structure2", 3)
-
-    println(structure1)
-    println(structure2)
-    println()
-
-    setPositions(structure1, "test2.pdb")
-    setPositions(structure2, "test2.pdb")
+    structure1 = Structure("structure1", 2, "test2.pdb")
+    structure2 = Structure("structure2", 3, "test2.pdb")
 
     println(structure1)
     println(structure2)
